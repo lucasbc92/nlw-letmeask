ca088849b65663453274a75b4db265dabca706e1
Lucas Bueno Cesario <lbc92@hotmail.com>
2021-06-25 16:00:59 -0300

    Project finished, preparing to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/rooms/new" component={NewRoom} exact />
+          <Route path="/rooms/:id" component={Room} exact />
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
